20bbf8296ea9f6f3f8f941a88025165f08e392a4
rissole <bonzaibrother@gmail.com>
2013-01-04 21:26:21 +1100

    change to use txt file battle log

--- Example.java
+++ Example.java
@@ -1,9 +1,11 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -11,12 +13,11 @@ import seleniumhelper.ShowdownHelper;
 
 public class Example  {
     public static void main(String[] args) throws Exception {
-    	WebDriver driver = new FirefoxDriver();
+    	//WebDriver driver = new FirefoxDriver();
     	// wait up to 10 seconds for elements to load
-    	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        
-        ShowdownHelper showdown = new ShowdownHelper(driver);
-        showdown.open();
+    	//driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        ShowdownHelper showdown = new ShowdownHelper(null);
+        /*showdown.open();
         String[] userPass = loadUserPass();
         showdown.login(userPass[0], userPass[1]);
         showdown.findBattle("Random Battle", "");
@@ -25,38 +26,44 @@ public class Example  {
         
         System.out.println("My name is " + showdown.getUserName() + ", and I just started a battle.");
         System.out.println("This is my team. There is none like it-");
-        List<String> team = showdown.getTeam(showdown.getUserName());
-        for (int i = 0; i < team.size(); ++i) {
-        	System.out.print(team.get(i));
-        	if (i != team.size()-1)
+        List<String> ourTeam = showdown.getTeam(showdown.getUserName());
+        for (int i = 0; i < ourTeam.size(); ++i) {
+        	System.out.print(ourTeam.get(i));
+        	if (i != ourTeam.size()-1)
         		System.out.print(", ");
         }
         System.out.println();
         System.out.println("My hapless opponent is " + showdown.getOpponentName() + ", and this is his team; or what I know of it:");
-        team = showdown.getTeam(showdown.getOpponentName());
+        List<String> team = showdown.getTeam(showdown.getOpponentName());
         for (int i = 0; i < team.size(); ++i) {
         	System.out.print(team.get(i));
         	if (i != team.size()-1)
         		System.out.print(", ");
         }
-        System.out.println();
-        
-        System.out.println("----------------");
+        System.out.println();*/
+             
+        System.out.println("-Turn 1 text---------------");
         System.out.println(showdown.getCurrentTurnText());
-        System.out.println("----------------");
+        System.out.println("-Turn 0 text---------------");
         System.out.println(showdown.getLastTurnText());
         System.out.println("----------------");
-        System.out.println(showdown.getCurrentPokemon("Opponent's Pokemon: "+showdown.getCurrentPokemon(showdown.getOpponentName())));
-        
-        //TODO: test waitForNextTurn, doMove.
-        
-        System.out.println("Moves:");
+        System.out.println("Opponent's Pokemon: "+showdown.getCurrentPokemon(showdown.getOpponentName(), false));
+             
+        /*System.out.println("Moves:");
         printlist(showdown.getMoves());
         for (String move : showdown.getMoves()) {
         	System.out.println(move + ": " + showdown.getMoveRemainingPP(move) + " PP");
         }
         
-        showdown.leaveBattle();
+        String switchingTo = ourTeam.get((new Random()).nextInt(6));
+        System.out.println("Switching to " + switchingTo);
+        showdown.switchTo(switchingTo,false);
+        
+        System.out.println(showdown.waitForNextTurn(0));
+        
+        System.out.println("Current Pokemon now (should be "+switchingTo+"): "+showdown.getCurrentPokemon(false));
+        
+        showdown.leaveBattle();*/
     }
     
     public static String[] loadUserPass() throws FileNotFoundException {
